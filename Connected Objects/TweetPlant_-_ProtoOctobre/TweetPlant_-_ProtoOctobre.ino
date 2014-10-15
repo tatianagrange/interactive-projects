8251995f1c4460caa0414381d01f7d2e130fd104
unknown <contact@tgrange.com>
2014-10-15 12:55:22 +0200

    Work. Send a tweet each 24 secondes

--- Connected Objects/TweetPlant_-_ProtoOctobre/TweetPlant_-_ProtoOctobre.ino
+++ Connected Objects/TweetPlant_-_ProtoOctobre/TweetPlant_-_ProtoOctobre.ino
@@ -5,28 +5,40 @@
 
 #include <Bridge.h>
 #include <Temboo.h>
+#include <Console.h>
 #include "TembooAccount.h"
 #include "TwitterAccount.h"
 
 int sensorPin = A1;
 int sensorValue = 0;
 
-void setup() {
-  Serial.begin(9600);
+long lastTweetTime = 0;
+long howManyTimeByDay = 3600;
+long oneDay = 86400;
 
-  // for debugging, wait until a serial console is connected
-  delay(4000);
-  while(!Serial);
-  pinMode(13, OUTPUT);
-  Bridge.begin();
+int tweetsNumber = 0;
+
+void setup() {
+	pinMode(13, OUTPUT);
+	Bridge.begin();
+	Console.begin();
+
+	while (!Console);
+	Console.println("You're connected to the Console!!!!");
+
+	digitalWrite(13, HIGH);
+	delay(1000);
+	digitalWrite(13, LOW);
 }
 
 void loop()
 {
-    sensorValue = analogRead(sensorPin);
+	sensorValue = analogRead(sensorPin);
 	delay(1000);
-	if(sensorValue < 600)  
+	if(sensorValue < 600 && timeIsChek())  
 	{
+                tweetsNumber++;
+                
 		// Temboo Configuration
 		TembooChoreo StatusesUpdateChoreo;
 		StatusesUpdateChoreo.begin();
@@ -41,28 +53,108 @@ void loop()
 		StatusesUpdateChoreo.addInput("ConsumerKey", TWITTER_API_KEY);    
 		StatusesUpdateChoreo.addInput("ConsumerSecret", TWITTER_API_SECRET);
 
-		String tweetText("J'ai soif!!");
-		StatusesUpdateChoreo.addInput("StatusUpdate", tweetText);
-    	        unsigned int returnCode = StatusesUpdateChoreo.run();
+                lastTweetTime = getTimeStamp();
+                String tweetText(makeTweet());
 
-      	        if (returnCode == 0) {
-      	            Serial.println("Success! Tweet sent!");
-                    digitalWrite(13, HIGH);
-      	        } else {
-      	          while (StatusesUpdateChoreo.available()) {
-      	            char c = StatusesUpdateChoreo.read();
-      	            Serial.print(c);
-      	          }
-      	        }
-      
-      	        StatusesUpdateChoreo.close();
+		StatusesUpdateChoreo.addInput("StatusUpdate", tweetText);
+		unsigned int returnCode = StatusesUpdateChoreo.run();
+                
+		if (returnCode == 0) {
+			Console.println("Success! Tweet sent => " + tweetText);
+			digitalWrite(13, HIGH);
+                        
+		} else {
+			while (StatusesUpdateChoreo.available()) {
+				char c = StatusesUpdateChoreo.read();
+  				Console.print(c);
+			}
+
+			digitalWrite(13, HIGH);
+			delay(500);
+			digitalWrite(13, LOW);
+			delay(500);
+			digitalWrite(13, HIGH);
+			delay(500);
+			digitalWrite(13, LOW);
+			delay(500);
+			digitalWrite(13, HIGH);
+			delay(500);
+			digitalWrite(13, LOW);
+			delay(500);
+			digitalWrite(13, HIGH);
+			delay(500);
+			digitalWrite(13, LOW);
+			delay(500);
+		}
+
+		StatusesUpdateChoreo.close();
 
 	}
-	else{
-		Serial.println("Meme pas soif"); 
+	else if(sensorValue >= 600){
+		Console.println("Meme pas soif"); 
+                tweetsNumber = 0;
 	}
 
         delay(1000);
         digitalWrite(13, LOW);
-	delay(250000); //wait 5 min
+}
+
+long getTimeStamp() {
+        String result;
+        Process time;
+        time.begin("date");
+        time.addParameter("+%s"); 
+        time.run(); 
+        
+        while(time.available()>0) {
+                char c = time.read();
+                if(c != '\n')
+                        result += c;
+        }
+        
+        return result.toInt();
+}
+
+String getDate() {
+        String result;
+        Process time;
+        time.begin("date");
+        time.addParameter("+%T"); 
+        time.run(); 
+        
+        while(time.available()>0) {
+                char c = time.read();
+                if(c != '\n')
+                        result += c;
+        }
+        
+        return result;
+}
+
+boolean timeIsChek(){
+        int dif = getTimeStamp() - lastTweetTime;
+        int maxTime = (int)(oneDay/howManyTimeByDay);
+        return dif > maxTime;
+}
+
+String makeTweet(){
+        String toReturn = "Il est "  + getDate();
+        switch(tweetsNumber){
+                case 1:
+                       toReturn += " et j'ai soif!";
+                       break; 
+                case 2:
+                       toReturn += " et j'ai encore soif!";
+                       break;
+                case 3:
+                       toReturn += " et j'ai toujours soif!";
+                       break;
+                case 4:
+                       toReturn += " et j'ai TOUJOURS soif!";
+                       break; 
+                default:
+                       toReturn += " et j'ai TOUJOURS soif... Help me!!!";
+                       break; 
+        }
+        return toReturn;
 }
